1b0759d21ba660e89d347a0cfd3d9c5e8f7938ca
DomamaN202 <vip.domaman@mail.ru>
2025-07-12 11:03:42 +0700

    Fix & Rebrand

--- minecraft/common/src/main/java/dev/kosmx/playerAnim/mixin/CapeLayerMixin.java
+++ minecraft/common/src/main/java/dev/kosmx/playerAnim/mixin/CapeLayerMixin.java
@@ -35,42 +35,42 @@ public abstract class CapeLayerMixin extends RenderLayer<PlayerRenderState, Play
         super(renderLayerParent);
     }
 
-    @Inject(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/renderer/entity/state/PlayerRenderState;FF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/HumanoidModel;renderToBuffer(Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;II)V"))
-    private void render(PoseStack poseStack, MultiBufferSource multiBufferSource, int i, PlayerRenderState playerRenderState, float f, float g, CallbackInfo ci) {
-        AnimationApplier emote = ((IPlayerAnimationState) playerRenderState).playerAnimator$getAnimationApplier();
-        if (model instanceof CapeLayerAccessor capeLayer) {
-            if (emote.isActive()) {
-                ModelPart torso = this.getParentModel().body;
-
-                poseStack.translate(torso.x / 16, torso.y / 16, torso.z / 16);
-                poseStack.mulPose((new Quaternionf()).rotateXYZ(torso.xRot, torso.yRot, torso.zRot));
-
-                poseStack.translate(0.0F, 0.0F, 0.125F);
-                poseStack.mulPose(Axis.YP.rotationDegrees(180));
-
-                ModelPart cape = capeLayer.getCape();
-                Vec3f transform = emote.get3DTransform(PartKey.CAPE, TransformType.POSITION, Vec3f.ZERO);
-                Vec3f rotation = emote.get3DTransform(PartKey.CAPE, TransformType.ROTATION, Vec3f.ZERO);
-                Vec3f scale = emote.get3DTransform(PartKey.CAPE, TransformType.SCALE, Vec3f.ONE);
-
-                cape.x = transform.getX();
-                cape.y = transform.getY();
-                cape.z = transform.getZ();
-                cape.xRot = rotation.getX();
-                cape.yRot = rotation.getY();
-                cape.zRot = rotation.getZ();
-                cape.xScale = scale.getX();
-                cape.yScale = scale.getY();
-                cape.zScale = scale.getZ();
-
-            }
-        }
-    }
-
-
-    @WrapWithCondition(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/renderer/entity/state/PlayerRenderState;FF)V", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;translate(FFF)V"))
-    private boolean translate(PoseStack instance, float f, float g, float h, @Local(argsOnly = true) PlayerRenderState playerRenderState) {
-        AnimationApplier emote = ((IPlayerAnimationState) playerRenderState).playerAnimator$getAnimationApplier();
-        return !emote.isActive();
-    }
+//    @Inject(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/renderer/entity/state/PlayerRenderState;FF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/HumanoidModel;renderToBuffer(Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;II)V"))
+//    private void render(PoseStack poseStack, MultiBufferSource multiBufferSource, int i, PlayerRenderState playerRenderState, float f, float g, CallbackInfo ci) {
+//        AnimationApplier emote = ((IPlayerAnimationState) playerRenderState).playerAnimator$getAnimationApplier();
+//        if (model instanceof CapeLayerAccessor capeLayer) {
+//            if (emote.isActive()) {
+//                ModelPart torso = this.getParentModel().body;
+//
+//                poseStack.translate(torso.x / 16, torso.y / 16, torso.z / 16);
+//                poseStack.mulPose((new Quaternionf()).rotateXYZ(torso.xRot, torso.yRot, torso.zRot));
+//
+//                poseStack.translate(0.0F, 0.0F, 0.125F);
+//                poseStack.mulPose(Axis.YP.rotationDegrees(180));
+//
+//                ModelPart cape = capeLayer.getCape();
+//                Vec3f transform = emote.get3DTransform(PartKey.CAPE, TransformType.POSITION, Vec3f.ZERO);
+//                Vec3f rotation = emote.get3DTransform(PartKey.CAPE, TransformType.ROTATION, Vec3f.ZERO);
+//                Vec3f scale = emote.get3DTransform(PartKey.CAPE, TransformType.SCALE, Vec3f.ONE);
+//
+//                cape.x = transform.getX();
+//                cape.y = transform.getY();
+//                cape.z = transform.getZ();
+//                cape.xRot = rotation.getX();
+//                cape.yRot = rotation.getY();
+//                cape.zRot = rotation.getZ();
+//                cape.xScale = scale.getX();
+//                cape.yScale = scale.getY();
+//                cape.zScale = scale.getZ();
+//
+//            }
+//        }
+//    }
+//
+//
+//    @WrapWithCondition(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/renderer/entity/state/PlayerRenderState;FF)V", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;translate(FFF)V"))
+//    private boolean translate(PoseStack instance, float f, float g, float h, @Local(argsOnly = true) PlayerRenderState playerRenderState) {
+//        AnimationApplier emote = ((IPlayerAnimationState) playerRenderState).playerAnimator$getAnimationApplier();
+//        return !emote.isActive();
+//    }
 }
